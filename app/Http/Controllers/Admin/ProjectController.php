41ae77797f75d497f98b7378a668505029f98266
Dong Huy <huytrinh109@gmail.com>
2024-10-08 09:22:15 +0700

    projects UI

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -7,6 +7,54 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
-    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return view('admin.projects.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        return view('admin.projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        //
+        return view('admin.projects.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
